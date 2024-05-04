809a75bb15d508168a4219ecc620f3c45a0b7e55
Leonel Sicaro <leonelsicaro@gmail.com>
2024-05-04 07:04:04 +0200

    falta css

--- client/emergencyProject/src/App.jsx
+++ client/emergencyProject/src/App.jsx
@@ -1,16 +1,18 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Axios from "axios"
 import './App.css'
 
 function App() {
-  const [nombre, setnombre] = useState('')
+  const [nombre, setNombre] = useState('')
   const [edad, setEdad] = useState(0)
   const [pais, setPais] = useState('')
   const [cargo, setCargo] = useState('')
   const [antiguedad, setAntiguedad] = useState(0)
+  const [id, setId] = useState('')
 
   const [usuariosList, setUsuariosList] = useState([])
+  const [editar, setEditar] = useState(false)
 
 
   const add = () => {
@@ -29,28 +31,92 @@ function App() {
       setUsuariosList(response.data)
     })
   }
-  getUsuarios()
+
+  const editarUsuario = (val) => {
+    setEditar(true)
+
+    setNombre(val.nombre)
+    setEdad(val.edad)
+    setPais(val.pais)
+    setCargo(val.cargo)
+    setAntiguedad(val.antiguedad)
+    setId(val.id)
+  }
+
+  const update = () => {
+    Axios.put("http://localhost:3000/update", {
+      id: id,
+      nombre: nombre,
+      edad: edad,
+      pais: pais,
+      cargo: cargo,
+      antiguedad: antiguedad
+    }).then(() => {
+      getUsuarios()
+    })
+  }
+  const deleteUser = (id) => {
+    Axios.delete(`http://localhost:3000/delete/${id}`, {
+
+    }).then(() => {
+      getUsuarios()
+    })
+  }
+
+  const cancelar=()=>{
+    setNombre('')
+    setEdad('')
+    setPais('')
+    setCargo('')
+    setAntiguedad('')
+    setId('')
+    setEditar(false)
+  }
+
+  useEffect(() => {
+
+    getUsuarios()
+  }, [])
 
   return (
     <>
       <div className="App">
         <div className="datos">
-          <label>Nombre: <input type="text" onChange={(e) => { setnombre(e.target.value) }} /></label><br />
-          <label>Edad: <input type="number" onChange={(e) => { setEdad(e.target.value) }} /></label><br />
-          <label>Pais: <input type="text" onChange={(e) => { setPais(e.target.value) }} /></label><br />
-          <label>Cargo: <input type="text" onChange={(e) => { setCargo(e.target.value) }} /></label><br />
-          <label>Antiguedad: <input type="number" onChange={(e) => { setAntiguedad(e.target.value) }} /></label><br />
-          <button onClick={add}>Registrar</button>
+          <label>Nombre: <input value={nombre} type="text" onChange={(e) => { setNombre(e.target.value) }} /></label><br />
+          <label>Edad: <input value={edad} type="number" onChange={(e) => { setEdad(e.target.value) }} /></label><br />
+          <label>Pais: <input value={pais} type="text" onChange={(e) => { setPais(e.target.value) }} /></label><br />
+          <label>Cargo: <input value={cargo} type="text" onChange={(e) => { setCargo(e.target.value) }} /></label><br />
+          <label>Antiguedad: <input value={antiguedad} type="number" onChange={(e) => { setAntiguedad(e.target.value) }} /></label><br />
+          {
+            editar == true ?
+              <>
+                <button onClick={update}>Actualizar</button>
+                <button onClick={cancelar}>Cancelar</button>
+              </>
+              :
+              <button onClick={add}>registrar</button>
+          }
         </div>
+
         <div className="lista">
-          
           {
             usuariosList.map((val, key) => {
-              return <div className="nombre">{val.nombre}</div>
+              return (
+                <div key={val.id} className="card">
+                  <div className="nombre" >{val.nombre}</div>
+                  <button onClick={() => { editarUsuario(val) }}>Actualizar datos</button>
+                  <button onClick={()=>{
+                    deleteUser(val.id)
+                  }}>eliminar</button>
+                  </div>
+                
+              )
+
             })
           }
         </div>
-      </div>
+
+      </div >
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -8,45 +8,78 @@ App.use(cors())
 App.use(express.json())
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"gluglu23",
-    database:"usuarios"
+    host: "localhost",
+    user: "root",
+    password: "gluglu23",
+    database: "usuarios"
 })
 
-App.post("/create",(req,res)=>{
-    const nombre= req.body.nombre;
-    const edad= req.body.edad;
-    const pais= req.body.pais;
-    const cargo= req.body.cargo;
-    const antiguedad= req.body.antiguedad;
+App.post("/create", (req, res) => {
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const pais = req.body.pais;
+    const cargo = req.body.cargo;
+    const antiguedad = req.body.antiguedad;
 
-    db.query('INSERT INTO usuarios(nombre,edad,pais,cargo,antiguedad) VALUES (?,?,?,?,?)',[nombre,edad,pais,cargo,antiguedad],
-(err,result)=>{
-    if(err){
-        console.log(err)
-    }else{
-        res.send("usuario registrado")
-    }
-}
-)
+    db.query('INSERT INTO usuarios(nombre,edad,pais,cargo,antiguedad) VALUES (?,?,?,?,?)', [nombre, edad, pais, cargo, antiguedad],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send("usuario registrado")
+            }
+        }
+    )
 })
-App.get("/usuarios",(req,res)=>{
-
-
-
+App.get("/usuarios", (req, res) => {
     db.query('SELECT * FROM usuarios',
-(err,result)=>{
-    if(err){
-        console.log(err)
-    }else{
-        res.send(result)
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+                }
+            }
+        )
     }
-}
 )
+App.put("/update", (req, res) => {
+    const id = req.body.id;
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const pais = req.body.pais;
+    const cargo = req.body.cargo;
+    const antiguedad = req.body.antiguedad;
+    
+
+    db.query('UPDATE usuarios SET nombre=?, edad=? ,pais=?, cargo=?, antiguedad=? WHERE id=?', [nombre, edad, pais, cargo, antiguedad,id],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send("usuario actualizado")
+            }
+        }
+    )
+})
+
+App.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    
+    
+
+    db.query('DELETE FROM usuarios WHERE id=?', [id],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        }
+    )
 })
 
 
-App.listen(3000,()=>{
+App.listen(3000, () => {
     console.log("listen in port 3000")
 })
